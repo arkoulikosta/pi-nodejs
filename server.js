1f46f5391f337f28b017375c4b8e7e8821f6e04c
arkoulikosta <arkoulikosta@yahoo.gr>
2017-12-22 14:06:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var port = 8080;
 var Gpio = require('onoff').Gpio;
 var sleep = require('sleep');
-var LED = new Gpio(4, 'out');
+var write_pin_1 = new Gpio(4, 'out');
+var write_pin_2 = new Gpio(17, 'out');
 var read_pin_1 = new Gpio(18, 'in');
+var read_pin_2 = new Gpio(23, 'in');
 
 var app = require('express')();
 var http = require('http').Server(app);
@@ -19,35 +21,40 @@ app.get('/', function(req, res){
 
 
 io.on('connection', function(socket){
-	//socket.emit();
-	console.log('new user');
-	io.emit('pin_status', read_pin_1.readSync());
-	console.log('pin_status' + read_pin_1.readSync());
 	
 	socket.on('led', function(msg){
 		console.log('func1');
-		LED.writeSync(msg);
-		io.emit('led', LED.readSync());
+		write_pin_1.writeSync(msg);
+		io.emit('led', write_pin_1.readSync());
 		sleep.msleep(200);
-		//LED.writeSync(0);
 		
 		console.log('entered led event ' + msg);
-		io.emit('led', LED.readSync());
-		console.log('emited ' + LED.readSync());
+		io.emit('led', write_pin_1.readSync());
+		console.log('emited ' + write_pin_1.readSync());
 	});
 	
 	socket.on('set_pin', function(msg){
 		console.log('set_pin: ' + msg);
-		LED.writeSync(1);
-		sleep.msleep(70);
-		LED.writeSync(0);
+		
+		if (msg===1){
+			write_pin_1.writeSync(1);
+			sleep.msleep(70);
+			write_pin_1.writeSync(0);
+		}
+		else if (msg === 2){
+			write_pin_2.writeSync(1);
+			sleep.msleep(70);
+			write_pin_2.writeSync(0);
+		}
+			
 	});
 	
 	
 	setInterval(function(){
-	io.emit('pin_status', read_pin_1.readSync());
-	//console.log('pin_status' + read_pin_1.readSync());
-	}, 500);
+	io.emit('pin_status', read_pin_1.readSync(), "1");
+	io.emit('pin_status', read_pin_2.readSync(), "2");
+	console.log('pin_status' + read_pin_2.readSync());
+	}, 2000);
 	
 });
 
